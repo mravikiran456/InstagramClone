862a876c5b09fd59afbc84e11716fd562de529a5
mravikiran456 <mravikiran456@gmail.com>
2022-12-07 14:06:09 +0530

    package changes

--- index.js
+++ index.js
@@ -16,4 +16,11 @@ app.get('/posts', (request, response) => {
   response.send(posts)
 })
 
-app.listen(3000)
+app.listen(3000,(err)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("listening on port 3000")
+    }
+})
