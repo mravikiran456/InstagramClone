fdbc2ae9e476a86ae974bb6a37874e0070430ff5
mravikiran456 <mravikiran456@gmail.com>
2022-12-09 12:38:29 +0530

    cors issue

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const express = require('express')
 const app = express()
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
+const serviceAccount = require('./serviceAccountKey.json');
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
 
 app.get('/posts', (request, response) => {
-    let posts=[
-        {
-            caption:'Golden Gate Bridge',
-            location:'Vizag , india',
-        },
-        {
-            caption:'London Eye',
-            location:'london, uk',
-        },
-    ]
-  response.send(posts)
+    response.set('Access-Control-Allow-Origin','* ')
+    let posts=[ ]
+     db.collection('posts'). orderBy('date', 'desc'). get().then(snapshot=>{
+        snapshot.forEach((doc) => {
+           posts.push(doc.data())
+          }); 
+          response.send(posts)
+     })
+
 })
 
+app.get('/createPost', (request, response) => {
+    response.set('Access-Control-Allow-Origin','* ')
+    response.send('createpost')
+
+})
 app.listen(3000,(err)=>{
     if(err){
         console.log(err);
